87797c355bdb1fd968b87729a493ceb2898febfe
Anton <office.sklyarov@gmail.com>
2020-10-22 14:09:28 +0300

    Updates

---
+++ precache-manifest.e066f45409c0ad9b8b89b7c869751bf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744d16e379322e040d66fd736d0c8918",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d99c1bf92843e0d248e",
+    "url": "./static/css/main.ede1571b.chunk.css"
+  },
+  {
+    "revision": "edcc8acd3c83803ea5af",
+    "url": "./static/js/2.18aeaae4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.18aeaae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d99c1bf92843e0d248e",
+    "url": "./static/js/main.4ccf4786.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
